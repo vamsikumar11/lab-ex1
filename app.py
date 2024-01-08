607adae7996df3caef2a02ead8d39be39007f1d2
vamsikumar11 <149361505+vamsikumar11@users.noreply.github.com>
2024-01-08 12:24:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        return render_template('success.html')
+    return render_template('register.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
